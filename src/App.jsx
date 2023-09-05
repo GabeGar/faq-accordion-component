015e461b99a0ec62bf6302a951b5f6452ce171c7
Gabegar <gagarcia190@gmail.com>
2023-09-05 12:58:44 -0400

    finalized desktop design w/ active states and added screenshots

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import mobileWoman from "./assets/images/illustration-woman-online-mobile.svg";
 import mobileShadow from "./assets/images/bg-pattern-mobile.svg";
+import desktopWoman from "./assets/images/illustration-woman-online-desktop.svg";
+import desktopBox from "./assets/images/illustration-box-desktop.svg";
+import desktopShadow from "./assets/images/bg-pattern-desktop.svg";
 
 import Page from "./components/Page";
 import FAQSection from "./components/FAQSection";
@@ -9,19 +12,48 @@ import FAQSection from "./components/FAQSection";
 import data from "./json/faq.json";
 
 const App = () => {
+    const [isViewPortMobile, setIsViewPortMobile] = useState(false);
     const [currentAnswerId, setCurrentAnswerID] = useState(null);
     const { faq } = data;
+
+    useEffect(() => {
+        const handleResize = (e) => {
+            e.target.innerWidth >= 986
+                ? setIsViewPortMobile(false)
+                : setIsViewPortMobile(true);
+        };
+
+        window.addEventListener("resize", handleResize);
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
 
     return (
         <Page>
             <main className="card">
-                <section className="illustration">
-                    <img
-                        src={mobileWoman}
-                        alt="Woman managing her emails on a large screen"
-                    />
-                    <img src={mobileShadow} alt="" />
-                </section>
+                {isViewPortMobile && (
+                    <section className="illustration">
+                        <img
+                            src={mobileWoman}
+                            alt="Woman managing her emails on a large screen"
+                        />
+                        <img src={mobileShadow} alt="" />
+                    </section>
+                )}
+                {!isViewPortMobile && (
+                    <section className="illustration-desktop">
+                        <div className="outer">
+                            <img
+                                src={desktopWoman}
+                                alt="Woman managing her emails on a large screen"
+                            />
+                            <img src={desktopShadow} alt="" />
+                            <img src={desktopBox} alt="" />
+                        </div>
+                    </section>
+                )}
                 <section className="faq">
                     <h1>FAQ</h1>
                     {faq.map((infoObj) => {
